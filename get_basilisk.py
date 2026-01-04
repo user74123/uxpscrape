ea9d79e2714b87cd492be8955f517ceb51c30595
user74123 <154016269+user74123@users.noreply.github.com>
2026-01-04 14:38:45 +0000

    new file:   .github/workflows/release.yml 	deleted:    README.md 	new file:   get_basilisk.py

---
+++ get_basilisk.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+BASE_URL = "https://o.rthost.win/basilisk/"
+INDEX_URL = f"{BASE_URL}index.php?sort=date&order=desc"
+
+def get_latest():
+    response = requests.get(INDEX_URL)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    rows = soup.find('div', class_='list').find('table').find('tbody').find_all('tr')
+
+    latest = {}
+    targets = ["basilisk52-g4.8.win32", "basilisk52-g4.8.win64"]
+
+    for row in rows:
+        name_cell = row.find('td', class_='n')
+
+        if not name_cell:
+            continue
+
+        link = name_cell.find('a')
+        filename = link.text.strip()
+        version = filename.split('-')[3]
+
+        for target in targets:
+            if filename.startswith(target) and filename.endswith("-xpmod.7z") and target not in latest:
+                latest[target] = {
+                    "filename": f"${target}.7z",
+                    "url": BASE_URL + filename,
+                    "version": version
+                }
+
+        if len(latest) == len(targets):
+            break
+
+    return latest
+
+if __name__ == "__main__":
+    meta = get_latest()
+    if len(meta) < 2:
+        print("Builds not found.")
+        exit(1)
+
+    if 'GITHUB_OUTPUT' in os.environ:
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
+            f.write(f"version={meta["basilisk52-g4.8.win32"]["version"]}\n")
+            f.write(f"win32_file={meta["basilisk52-g4.8.win32"]["filename"]}\n")
+            f.write(f"win32_url={meta["basilisk52-g4.8.win32"]["url"]}\n")
+            f.write(f"win64_file={meta["basilisk52-g4.8.win64"]["filename"]}\n")
+            f.write(f"win64_url={meta["basilisk52-g4.8.win64"]["url"]}\n")
